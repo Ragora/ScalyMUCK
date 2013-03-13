5a847dc69a9c559c37030c8325f99aaf8146f78d
Ragora <robertmacgregor1995@hotmail.com>
2013-03-13 12:17:17 -0400

    Mod loading changes

--- application/game/world.py
+++ application/game/world.py
@@ -15,45 +15,45 @@ from sqlalchemy.orm import scoped_session
 from models import Room, Player, Item
 
 class World():
-	database_engine = None
-	database_session = None
+	engine = None
+	session = None
 	
-	_cached_players = [ ]
-	_cached_items = [ ]
-	_cached_rooms = [ ]
+	cached_players = [ ]
+	cached_items = [ ]
+	cached_rooms = [ ]
 	def __init__(self, engine):
-		self.database_engine = engine
-		self.database_session = scoped_session(sessionmaker(bind=self.database_engine))
+		self.engine = engine
+		self.session = scoped_session(sessionmaker(bind=self.engine))
 		return
 	      
 	def create_room(self, name, description='<Unset>', owner=0):
 		room = Room(name, description, owner)
 		room.world = self
-		self.database_session.add(room)
-		self.database_session.commit()
+		self.session.add(room)
+		self.session.commit()
 
-		self._cached_rooms.append(room)
+		self.cached_rooms.append(room)
 		return room
 	      
 	def find_room(self, id=None, name=None):
 		if (name is None and id is None):
 			return
 
-		for room in self._cached_rooms:
+		for room in self.cached_rooms:
 			if (room.id == id or room.name == name):
 				return room
 
 		if (name is not None):
-			target_room = self.database_session.query.query(Room).filter_by(name=name).first()
+			target_room = self.session.query.query(Room).filter_by(name=name).first()
 			if (target_room is not None):
 				target_room.world = self
-				self._cached_rooms.append(target_room)
+				self.cached_rooms.append(target_room)
 				return target_room 
 		else:
-			target_room = self.database_session.query(Room).filter_by(id=id).first()
+			target_room = self.session.query(Room).filter_by(id=id).first()
 			if (target_room is not None):
 				target_room.world = self
-				self._cached_rooms.append(target_room)
+				self.cached_rooms.append(target_room)
 				return target_room
 		return None
 	
@@ -62,38 +62,38 @@ class World():
 			
 		player = Player(name, password, work_factor, location.id, 0)
 		player.world = self
-		self.database_session.add(player)
+		self.session.add(player)
 
 		location.players.append(player)
-		self.database_session.add(location)
+		self.session.add(location)
 
-		self.database_session.add(player_inventory)
-		self.database_session.commit()
+		self.session.add(player_inventory)
+		self.session.commit()
 		
 		player.location = location
-		self._cached_players.append(player)
+		self.cached_players.append(player)
 		return player
 	      
 	def find_player(self,id=None,name=None,display_name=None):
 		if (name is None and id is None):
 			return None
 
-		for player in self._cached_players:
+		for player in self.cached_players:
 			if (player.id == id or player.name == name or player.display_name == display_name):
 				return player
 				 
 		if (name is not None):
-			target_player = self.database_session.query(Player).filter_by(name=name).first()
+			target_player = self.session.query(Player).filter_by(name=name).first()
 			if (target_player is not None):
 				target_player.world = self
-				self._cached_players.append(target_player)
-				target_player.location = self.database_session.query(Room).filter_by(id=target_player.location_id).first()
+				self.cached_players.append(target_player)
+				target_player.location = self.session.query(Room).filter_by(id=target_player.location_id).first()
 				return target_player
 		else:
-			target_player = self.database_session.query(Player).filter_by(id=id).first()
+			target_player = self.session.query(Player).filter_by(id=id).first()
 			if (target_player is not None):
 				target_player.world = self
-				self._cached_players.append(target_player)
-				target_player.location = self.database_session.query(Room).filter_by(id=target_player.location_id).first()
+				self.cached_players.append(target_player)
+				target_player.location = self.session.query(Room).filter_by(id=target_player.location_id).first()
 				return target_player
 		return None
