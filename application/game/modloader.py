432a2f1935b1c56afa7753502a5940b4e67250e7
Ragora <robertmacgregor1995@hotmail.com>
2013-07-19 15:22:27 -0400

    Adjusted help to go by category so it is cleaner

--- application/game/modloader.py
+++ application/game/modloader.py
@@ -118,6 +118,7 @@ class ModLoader:
 		self.commands['mods']['usage'] = 'mods'
 		self.commands['mods']['privilege'] = 3
 		self.commands['mods']['modification'] = '<CORE>'
+		self.commands['mods']['category'] = 'Core'
 
 		self.commands['load'] = { }
 		self.commands['load']['command'] = self.command_load
@@ -125,6 +126,7 @@ class ModLoader:
 		self.commands['load']['usage'] = 'load <name>'
 		self.commands['load']['privilege'] = 3
 		self.commands['load']['modification'] = '<CORE>'
+		self.commands['load']['category'] = 'Core'
 
 		self.commands['unload'] = { }
 		self.commands['unload']['command'] = self.command_unload
@@ -132,6 +134,7 @@ class ModLoader:
 		self.commands['unload']['usage'] = 'unload <name>'
 		self.commands['unload']['privilege'] = 3
 		self.commands['unload']['modification'] = '<CORE>'
+		self.commands['unload']['category'] = 'Core'
 
 	def find_command(self, name):
 		""" Returns a command by name. """
